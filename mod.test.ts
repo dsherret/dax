e9816799bcc5944a47f072257a87babbcd42261f
Ken March <knmarch@implant.net>
2022-07-30 09:41:21 -0600

    feat: add `test` command (#6)

--- mod.test.ts
+++ mod.test.ts
@@ -176,6 +176,99 @@ Deno.test("sleep command", async () => {
   assertEquals(end - start > 190, true);
 });
 
+Deno.test("test command", async (t) => {
+  await Deno.writeFile('zero.dat', new Uint8Array());
+  await Deno.writeFile('non-zero.dat', new Uint8Array([242]));
+  if (Deno.build.os !== 'windows') {
+    await Deno.symlink('zero.dat', 'linked.dat');
+  }
+  
+  await t.step("test -e", async () => {
+    const result = await $`test -e zero.dat`.noThrow();
+    assertEquals(result.code, 0);
+  });
+  await t.step("test -f", async () => {
+    const result = await $`test -f zero.dat`.noThrow();
+    assertEquals(result.code, 0, "should be a file");
+  });
+  await t.step("test -f on non-file", async () => {
+    const result = await $`test -f ${Deno.cwd()}`.noThrow();
+    assertEquals(result.code, 1, "should not be a file");
+    assertEquals(result.stderr, "");
+  });
+  await t.step("test -d", async() => {
+    const result = await $`test -d ${Deno.cwd()}`.noThrow();
+    assertEquals(result.code, 0, `${Deno.cwd()} should be a directory`);
+  });
+  await t.step("test -d on non-directory", async() => {
+    const result = await $`test -d zero.dat`.noThrow();
+    assertEquals(result.code, 1, "should not be a directory");
+    assertEquals(result.stderr, "");
+  });
+  await t.step("test -s", async () => {
+    const result = await $`test -s non-zero.dat`.noThrow();
+    assertEquals(result.code, 0, "should be > 0");
+    assertEquals(result.stderr, "");
+  });
+  await t.step("test -s on zero-length file", async () => {
+    const result = await $`test -s zero.dat`.noThrow();
+    assertEquals(result.code, 1, "should fail as file is zero-sized");
+    assertEquals(result.stderr, "");
+  });
+  if (Deno.build.os !== 'windows') {
+    await t.step("test -L", async () => {
+      const result = await $`test -L linked.dat`.noThrow();
+      assertEquals(result.code, 0, "should be a symlink");
+    });
+  }
+  await t.step("test -L on a non-symlink", async () => {
+    const result = await $`test -L zero.dat`.noThrow();
+    assertEquals(result.code, 1, "should fail as not a symlink");
+    assertEquals(result.stderr, "");    
+  });
+  await t.step("should error on unsupported test type", async () => {
+    const result = await $`test -z zero.dat`.noThrow();
+    assertEquals(result.code, 2, "should have exit code 2");
+    assertEquals(result.stderr, "test: unsupported test type\n");
+  });
+  await t.step("should error with not enough arguments", async() => {
+    const result = await $`test`.noThrow();
+    assertEquals(result.code, 2, "should have exit code 2");
+    assertEquals(result.stderr, "test: expected 2 arguments\n");
+  });
+  await t.step("should error with too many arguments", async() => {
+    const result = await $`test -f a b c`.noThrow();
+    assertEquals(result.code, 2, "should have exit code 2");
+    assertEquals(result.stderr, "test: expected 2 arguments\n");
+  });
+  await t.step("should work with boolean: pass && ..", async () => {
+    const result = await $`test -f zero.dat && echo yup`.noThrow();
+    assertEquals(result.code, 0);
+    assertEquals(result.stdout, "yup\n");
+  });
+  await t.step("should work with boolean: fail && ..", async () => {
+    const result = await $`test -f ${Deno.cwd()} && echo nope`.noThrow();
+    assertEquals(result.code, 1), "should have exit code 1";
+    assertEquals(result.stdout, "");
+  });
+  await t.step("should work with boolean: pass || ..", async () => {
+    const result = await $`test -f zero.dat || echo nope`.noThrow();
+    assertEquals(result.code, 0);
+    assertEquals(result.stdout, "");
+  });
+  await t.step("should work with boolean: fail || ..", async () => {
+    const result = await $`test -f ${Deno.cwd()} || echo yup`.noThrow();
+    assertEquals(result.code, 0);
+    assertEquals(result.stdout, "yup\n");
+  });
+
+  if (Deno.build.os !== 'windows') {
+    await Deno.remove('linked.dat');
+  }
+  await Deno.remove('zero.dat');
+  await Deno.remove('non-zero.dat');
+});
+
 Deno.test("exit command", async() => {
   {
     const result = await $`exit`.noThrow();
