7325878f0100fc8b63ff59c4456924d38801fb2f
David Sherret <dsherret@gmail.com>
2022-10-18 21:07:04 -0400

    feat: upgrade deno_std to 0.160.0

--- src/deps.test.ts
+++ src/deps.test.ts
@@ -1,1 +1,1 @@
-export { assert, assertEquals, assertRejects, assertThrows } from "https://deno.land/std@0.158.0/testing/asserts.ts";
+export { assert, assertEquals, assertRejects, assertThrows } from "https://deno.land/std@0.160.0/testing/asserts.ts";
