662ca7c67e620b7ed92acd2e5867b2aed58b049d
David Sherret <dsherret@gmail.com>
2022-12-28 14:24:12 -0500

    Rendering tests

--- src/console/progress/mod.ts
+++ src/console/progress/mod.ts
@@ -1,4 +1,5 @@
 import { colors } from "../../deps.ts";
+import { ConsoleSize, TextItem } from "../utils.ts";
 import { addProgressBar, DrawIntervalProgressBar, removeProgressBar } from "./interval.ts";
 
 export interface ProgressOptions {
@@ -8,12 +9,7 @@ export interface ProgressOptions {
 }
 
 export class ProgressBar {
-  #state: {
-    message: string | undefined;
-    prefix: string | undefined;
-    length: number | undefined;
-    currentPos: number;
-  };
+  #state: RenderState;
   #pb: DrawIntervalProgressBar;
   #withCount = 0;
 
@@ -23,52 +19,11 @@ export class ProgressBar {
       prefix: opts.prefix,
       length: opts.length,
       currentPos: 0,
+      tickCount: 0,
     };
-    const tickStrings = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
-    let i = 0;
     this.#pb = addProgressBar((size) => {
-      if (this.#state.length == null || this.#state.length === 0) {
-        i = (i + 1) % tickStrings.length;
-        let text = "";
-        if (this.#state.prefix != null) {
-          text += `${colors.green(this.#state.prefix)} `;
-        }
-        text += colors.green(tickStrings[i]);
-        if (this.#state.message != null) {
-          text += ` ${this.#state.message}`;
-        }
-        return [text];
-      } else {
-        let firstLine = "";
-        if (this.#state.prefix != null) {
-          firstLine += colors.green(this.#state.prefix);
-        }
-        if (this.#state.message != null) {
-          if (firstLine.length > 0) {
-            firstLine += " ";
-          }
-          firstLine += this.#state.message;
-        }
-        const percent = Math.min(this.#state.currentPos / this.#state.length, 1);
-        const maxWidth = Math.max(10, Math.min(75, size.columns - 5));
-        const sameLineTextWidth = 6 + this.#state.length.toString().length * 2;
-        const totalBars = Math.max(1, maxWidth - sameLineTextWidth);
-        const completedBars = Math.floor(totalBars * percent);
-        let secondLine = "";
-
-        secondLine += "[";
-        if (completedBars != totalBars) {
-          if (completedBars > 0) {
-            secondLine += colors.cyan("#".repeat(completedBars - 1) + ">");
-          }
-          secondLine += colors.blue("-".repeat(totalBars - completedBars));
-        } else {
-          secondLine += colors.blue("#".repeat(completedBars));
-        }
-        secondLine += `] (${this.#state.currentPos}/${this.#state.length})`;
-
-        return [firstLine, secondLine];
-      }
+      this.#state.tickCount++;
+      return renderProgressBar(this.#state, size);
     });
   }
 
@@ -107,4 +62,62 @@ export class ProgressBar {
 
 export function progress(opts: ProgressOptions) {
   return new ProgressBar(opts);
+}
+
+interface RenderState {
+  message: string | undefined;
+  prefix: string | undefined;
+  length: number | undefined;
+  currentPos: number;
+  tickCount: number;
+}
+
+export function renderProgressBar(state: RenderState, size: ConsoleSize): TextItem[] {
+  if (state.length == null || state.length === 0) {
+    const tickStrings = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+    let text = "";
+    if (state.prefix != null) {
+      text += `${colors.green(state.prefix)} `;
+    }
+    text += colors.green(tickStrings[Math.abs(state.tickCount) % tickStrings.length]);
+    if (state.message != null) {
+      text += ` ${state.message}`;
+    }
+    return [text];
+  } else {
+    let firstLine = "";
+    if (state.prefix != null) {
+      firstLine += colors.green(state.prefix);
+    }
+    if (state.message != null) {
+      if (firstLine.length > 0) {
+        firstLine += " ";
+      }
+      firstLine += state.message;
+    }
+    const percent = Math.min(state.currentPos / state.length, 1);
+    const maxWidth = Math.max(10, Math.min(75, size.columns - 5));
+    const sameLineTextWidth = 6 + state.length.toString().length * 2;
+    const totalBars = Math.max(1, maxWidth - sameLineTextWidth);
+    const completedBars = Math.floor(totalBars * percent);
+    let secondLine = "";
+
+    secondLine += "[";
+    if (completedBars != totalBars) {
+      if (completedBars > 0) {
+        secondLine += colors.cyan("#".repeat(completedBars - 1) + ">");
+      }
+      secondLine += colors.blue("-".repeat(totalBars - completedBars));
+    } else {
+      secondLine += colors.blue("#".repeat(completedBars));
+    }
+    secondLine += `] (${state.currentPos}/${state.length})`;
+
+    const result = [];
+    if (firstLine.length > 0) {
+      result.push(firstLine);
+    }
+    result.push(secondLine);
+    return result;
+  }
 }
