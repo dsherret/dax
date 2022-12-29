6180a9f7402927a43c81b4a448046317f9b44311
David Sherret <dsherret@users.noreply.github.com>
2022-12-29 14:53:19 -0500

    fix: better handling for if terminal supports progress bars and selecion (#58)

--- src/console/logger.ts
+++ src/console/logger.ts
@@ -12,12 +12,14 @@ const refreshItems: Record<LoggerRefreshItemKind, TextItem[] | undefined> = {
 
 function setItems(kind: LoggerRefreshItemKind, items: TextItem[] | undefined, size?: ConsoleSize) {
   refreshItems[kind] = items;
-  return refresh(size);
+  refresh(size);
 }
 
-async function refresh(size?: ConsoleSize) {
-  const staticText = await getStaticText();
-  refreshWithStaticText(staticText, size);
+function refresh(size?: ConsoleSize) {
+  const staticText = getStaticTextIfCreated();
+  if (staticText) {
+    refreshWithStaticText(staticText, size);
+  }
 }
 
 function refreshWithStaticText(staticText: Awaited<ReturnType<typeof getStaticText>>, size?: ConsoleSize) {
@@ -44,7 +46,17 @@ async function logOnce(items: TextItem[], size?: ConsoleSize) {
   }, size);
 }
 
+async function ensureInitialized() {
+  await getStaticText();
+}
+
+function isInitilaized() {
+  return getStaticTextIfCreated() != null;
+}
+
 const logger = {
+  ensureInitialized,
+  isInitilaized,
   setItems,
   logOnce,
   logAboveStaticText,
