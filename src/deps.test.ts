0b9eadcde6d81a4ee2c8aa35867fd2a6946b1066
David Sherret <dsherret@gmail.com>
2022-12-29 11:25:51 -0500

    feat: $.request - `pipeTo` and `pipeThrough`

--- src/deps.test.ts
+++ src/deps.test.ts
@@ -5,3 +5,4 @@ export {
   assertStringIncludes,
   assertThrows,
 } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+export { writableStreamFromWriter } from "https://deno.land/std@0.167.0/streams/writable_stream_from_writer.ts";
