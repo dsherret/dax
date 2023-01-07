29aad9459b0a234a5cb5f9044c27694e8fa9be2e
David Sherret <dsherret@users.noreply.github.com>
2023-01-07 16:45:03 -0500

    feat: upgrade to deno_std 0.171 (#75)

--- src/deps.test.ts
+++ src/deps.test.ts
@@ -4,6 +4,6 @@ export {
   assertRejects,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.170.0/testing/asserts.ts";
-export { writableStreamFromWriter } from "https://deno.land/std@0.170.0/streams/writable_stream_from_writer.ts";
-export { serve } from "https://deno.land/std@0.170.0/http/server.ts";
+} from "https://deno.land/std@0.171.0/testing/asserts.ts";
+export { writableStreamFromWriter } from "https://deno.land/std@0.171.0/streams/writable_stream_from_writer.ts";
+export { serve } from "https://deno.land/std@0.171.0/http/server.ts";
--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,10 @@
-export * as colors from "https://deno.land/std@0.170.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.170.0/fs/mod.ts";
-export { Buffer } from "https://deno.land/std@0.170.0/io/buffer.ts";
-export { BufReader } from "https://deno.land/std@0.170.0/io/buf_reader.ts";
-export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
-export { readAll } from "https://deno.land/std@0.170.0/streams/read_all.ts";
-export { writeAllSync } from "https://deno.land/std@0.170.0/streams/write_all.ts";
+export * as colors from "https://deno.land/std@0.171.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.171.0/fs/mod.ts";
+export { Buffer } from "https://deno.land/std@0.171.0/io/buffer.ts";
+export { BufReader } from "https://deno.land/std@0.171.0/io/buf_reader.ts";
+export * as path from "https://deno.land/std@0.171.0/path/mod.ts";
+export { readAll } from "https://deno.land/std@0.171.0/streams/read_all.ts";
+export { writeAllSync } from "https://deno.land/std@0.171.0/streams/write_all.ts";
 export { default as localDataDir } from "https://deno.land/x/dir@1.5.1/data_local_dir/mod.ts";
 export { outdent } from "https://deno.land/x/outdent@v0.8.0/src/index.ts";
 export { RealEnvironment as DenoWhichRealEnvironment, which, whichSync } from "https://deno.land/x/which@0.2.1/mod.ts";
