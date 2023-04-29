06c07ddeb29dfadb223ae0176164da0c94309bb4
David Sherret <dsherret@users.noreply.github.com>
2023-04-29 13:29:57 -0400

    fix: use `Deno.Command` internally instead of `Deno.run` (#138)

--- src/deps.test.ts
+++ src/deps.test.ts
@@ -7,6 +7,7 @@ export {
   assertStringIncludes,
   assertThrows,
 } from "https://deno.land/std@0.182.0/testing/asserts.ts";
+export { readerFromStreamReader } from "https://deno.land/std@0.182.0/streams/reader_from_stream_reader.ts";
 export { writableStreamFromWriter } from "https://deno.land/std@0.182.0/streams/writable_stream_from_writer.ts";
 export { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
