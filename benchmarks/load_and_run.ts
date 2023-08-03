ec5ac523fc9d82f65de7032804571e0efa603ec1
Cody Casterline <cody.casterline@gmail.com>
2023-08-02 20:49:00 -0700

    chore: add basic benchmark

---
+++ benchmarks/load_and_run.ts
@@ -0,0 +1,9 @@
+/**
+ * Used by mod.bench.ts to test how long it takes to load 
+ * the wasm module.
+ * @module
+ */
+
+import $ from "../mod.ts"
+
+await $`echo "Hello, world!"`.text()
---
+++ benchmarks/mod.bench.ts
@@ -0,0 +1,5 @@
+import $ from "../mod.ts"
+
+Deno.bench(async function loadWasmModule() {
+    await $`deno run -A benchmarks/load_and_run.ts`
+})
