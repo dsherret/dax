b9397a52d903202b22809f22d275482e43d5720d
Cody Casterline <cody.casterline@gmail.com>
2023-08-05 07:20:30 -0700

    fix: don't require `--allow-net` (#158)

--- src/deps.ts
+++ src/deps.ts
@@ -6,6 +6,5 @@ export * as path from "https://deno.land/std@0.182.0/path/mod.ts";
 export { readAll } from "https://deno.land/std@0.182.0/streams/read_all.ts";
 export { readerFromStreamReader } from "https://deno.land/std@0.182.0/streams/reader_from_stream_reader.ts";
 export { writeAll, writeAllSync } from "https://deno.land/std@0.182.0/streams/write_all.ts";
-export { default as localDataDir } from "https://deno.land/x/dir@1.5.1/data_local_dir/mod.ts";
 export { outdent } from "https://deno.land/x/outdent@v0.8.0/src/index.ts";
 export { RealEnvironment as DenoWhichRealEnvironment, which, whichSync } from "https://deno.land/x/which@0.3.0/mod.ts";
