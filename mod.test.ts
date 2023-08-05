bb4851e4a9a27dac7be99afa2db6c168eae11c42
David Sherret <dsherret@gmail.com>
2023-08-05 16:06:33 -0400

    Update

--- mod.test.ts
+++ mod.test.ts
@@ -1514,15 +1514,15 @@ Deno.test("should error creating a command signal", () => {
   );
 });
 
-Deno.test("should receive signal when listening", { ignore: Deno.build.os === "windows" }, async () => {
+Deno.test("should receive signal when listening", { ignore: Deno.build.os !== "linux" }, async () => {
   const p =
     $`deno eval 'Deno.addSignalListener("SIGINT", () => console.log("RECEIVED SIGINT")); console.log("started"); setTimeout(() => {}, 10_000)'`
       .noThrow()
       .stdout("piped")
       .spawn();
-  await $.sleep(50);
+  await $.sleep(60);
   p.kill("SIGINT");
-  await $.sleep(20);
+  await $.sleep(30);
   // now terminate it
   p.kill("SIGKILL");
   assertEquals((await p).stdout, "started\nRECEIVED SIGINT\n");
