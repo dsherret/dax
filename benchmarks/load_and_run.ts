f7e10f2156c68bb8382a492c4b4a68fbc527abfa
David Sherret <dsherret@gmail.com>
2023-08-05 10:16:30 -0400

    I verified these benchmarks, so let's remove it now.

--- benchmarks/load_and_run.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Used by mod.bench.ts to test how long it takes to load
- * the wasm module.
- * @module
- */
-
-import $ from "../mod.ts";
-
-await $`echo "Hello, world!"`.text();
--- benchmarks/mod.bench.ts
+++
@@ -1,5 +0,0 @@
-import $ from "../mod.ts";
-
-Deno.bench(async function loadWasmModule() {
-  await $`deno run -A benchmarks/load_and_run.ts`;
-});
