a41ffe632f002b51d290f89ff1c2c3259804ac32
David Sherret <dsherret@users.noreply.github.com>
2023-12-17 11:35:25 +0100

    feat: upgrade to deno_std 0.209 (#198)

--- src/deps.test.ts
+++ src/deps.test.ts
@@ -7,10 +7,10 @@ export {
   assertRejects,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.201.0/assert/mod.ts";
-export { readerFromStreamReader } from "https://deno.land/std@0.201.0/streams/reader_from_stream_reader.ts";
-export { writableStreamFromWriter } from "https://deno.land/std@0.201.0/streams/writable_stream_from_writer.ts";
-export { serve } from "https://deno.land/std@0.201.0/http/server.ts";
+} from "https://deno.land/std@0.209.0/assert/mod.ts";
+export { readerFromStreamReader } from "https://deno.land/std@0.209.0/streams/reader_from_stream_reader.ts";
+export { writableStreamFromWriter } from "https://deno.land/std@0.209.0/streams/writable_stream_from_writer.ts";
+export { serve } from "https://deno.land/std@0.209.0/http/server.ts";
 
 /**
  * Creates a temporary directory, changes the cwd to this directory,
