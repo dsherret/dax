9f063c2092edfa2a4d48a5d1984a5238f024c83d
David Sherret <dsherret@gmail.com>
2023-12-17 10:54:10 +0100

    Add test

---
+++ src/command.test.ts
@@ -0,0 +1,7 @@
+import { escapeArg } from "./command.ts";
+import { assertEquals } from "./deps.test.ts";
+
+Deno.test("escapes arg", () => {
+  assertEquals(escapeArg("hello"), "hello");
+  assertEquals(escapeArg("'abc'"), `''"'"'abc'"'"''`);
+});
