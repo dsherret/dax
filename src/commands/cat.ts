8717849b31f03c37b7640d00f1e8b12dc62cfd40
David Sherret <dsherret@gmail.com>
2024-01-26 18:02:22 -0500

    feat: support pipe sequences

--- src/commands/cat.ts
+++ src/commands/cat.ts
@@ -21,17 +21,18 @@ export async function catCommand(
 
 async function executeCat(context: CommandContext) {
   const flags = parseCatArgs(context.args);
-  let exit_code = 0;
+  let exitCode = 0;
   const buf = new Uint8Array(1024);
 
   for (const path of flags.paths) {
     if (path === "-") { // read from stdin
       if (typeof context.stdin === "object") { // stdin is a Reader
-        while (true) {
+        while (!context.signal.aborted) {
           const size = await context.stdin.read(buf);
           if (!size || size === 0) break;
           else context.stdout.writeSync(buf.slice(0, size));
         }
+        exitCode = context.signal._abortedExitCode;
       } else {
         const _assertValue: "null" | "inherit" = context.stdin;
         throw new Error(`not supported. stdin was '${context.stdin}'`);
@@ -40,21 +41,22 @@ async function executeCat(context: CommandContext) {
       let file;
       try {
         file = await Deno.open(pathUtils.join(context.cwd, path), { read: true });
-        while (true) {
+        while (!context.signal.aborted) {
           // NOTE: rust supports cancellation here
           const size = file.readSync(buf);
           if (!size || size === 0) break;
           else context.stdout.writeSync(buf.slice(0, size));
         }
+        exitCode = context.signal._abortedExitCode;
       } catch (err) {
         context.stderr.writeLine(`cat ${path}: ${err}`);
-        exit_code = 1;
+        exitCode = 1;
       } finally {
         if (file) file.close();
       }
     }
   }
-  return exit_code;
+  return exitCode;
 }
 
 export function parseCatArgs(args: string[]): CatFlags {
