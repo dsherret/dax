4a422da6d8f07331e080836aceaf092d62ba275f
David Sherret <dsherret@gmail.com>
2024-01-27 21:56:11 -0500

    Rename symbols

--- src/common.ts
+++ src/common.ts
@@ -17,7 +17,7 @@ export const symbols = {
    * await $`echo 1 > ${myObj}`;
    * ```
    */
-  writable: Symbol.for("dax.OutputWritable"),
+  writable: Symbol.for("dax.writable"),
   /** Use this symbol to enable the provided object to be read from in
    * an input redirect within a template literal expression.
    *
@@ -32,7 +32,7 @@ export const symbols = {
    * await $`gzip < ${myObj}`;
    * ```
    */
-  readable: Symbol.for("dax.InputReadable"),
+  readable: Symbol.for("dax.readable"),
 };
 
 /**
