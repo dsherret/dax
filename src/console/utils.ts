fac338ccc7d2ecb3741954263aa29b057bbd582e
berlysia <daisuke_shiohara@dwango.co.jp>
2025-04-19 00:18:34 +0900

    fix: handle multibyte characters in `$.prompt` (#319)

--- src/console/utils.ts
+++ src/console/utils.ts
@@ -2,7 +2,6 @@ import { wasmInstance } from "../lib/mod.ts";
 import { logger, LoggerRefreshItemKind } from "./logger.ts";
 
 const encoder = new TextEncoder();
-const decoder = new TextDecoder();
 
 export enum Keys {
   Up,
@@ -15,10 +14,18 @@ export enum Keys {
 }
 
 export async function* readKeys() {
+  return yield* innerReadKeys(Deno.stdin);
+}
+
+export async function* innerReadKeys(reader: Pick<typeof Deno.stdin, "read">) {
   const { strip_ansi_codes } = wasmInstance;
+
+  // A new decoder is always needed to take into account that UTF-8 sequences are read in pieces.
+  const decoder = new TextDecoder();
+
   while (true) {
     const buf = new Uint8Array(8);
-    const byteCount = await Deno.stdin.read(buf);
+    const byteCount = await reader.read(buf);
     if (byteCount == null) {
       break;
     }
@@ -53,7 +60,9 @@ export async function* readKeys() {
         continue;
       }
     }
-    const text = strip_ansi_codes(decoder.decode(buf.slice(0, byteCount ?? 0)));
+    // stream: true preserves the remaining bytes that the decoder did not interpret as characters.
+    // This is important because we want to handle multibyte characters correctly.
+    const text = strip_ansi_codes(decoder.decode(buf.slice(0, byteCount ?? 0), { stream: true }));
     if (text.length > 0) {
       yield text;
     }
