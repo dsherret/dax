aea85f7de029942b062557213246cf1f2846cadc
David Sherret <dsherret@gmail.com>
2025-04-18 13:19:50 -0400

    do not allocate a string

--- src/rs_lib/src/lib.rs
+++ src/rs_lib/src/lib.rs
@@ -12,7 +12,7 @@ use wasm_bindgen::prelude::*;
 pub fn parse(command: String) -> Result<JsValue, JsValue> {
   console_error_panic_hook::set_once();
 
-  match deno_task_shell::parser::parse(&command) {
+  match deno_task_shell::parser::parse(command.trim()) {
     Ok(list) => Ok(serde_wasm_bindgen::to_value(&list).unwrap()),
     Err(err) => Err(serde_wasm_bindgen::to_value(&format!("{}", err)).unwrap()),
   }
