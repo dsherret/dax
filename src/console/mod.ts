8b8d8f6142f8e735bcf2b4f1a4f5746ddd3de5e0
David Sherret <dsherret@gmail.com>
2026-04-21 17:36:16 -0400

    update

--- src/console/mod.ts
+++ src/console/mod.ts
@@ -1,6 +1,5 @@
 export { confirm, maybeConfirm } from "./confirm.ts";
 export type { ConfirmOptions } from "./confirm.ts";
-export { logger } from "./logger.ts";
 export { maybeMultiSelect, multiSelect } from "./multiSelect.ts";
 export type { MultiSelectOption, MultiSelectOptions } from "./multiSelect.ts";
 export type { ProgressOptions } from "./progress.ts";
